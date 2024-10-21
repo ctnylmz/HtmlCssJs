0a14e24d4fcc5dc762aef897e5243b1dbc88074e
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-10-21 15:56:44 +0300

    Javascript IF ELSE

--- Dersler/1/assets/app.js
+++ Dersler/1/assets/app.js
@@ -1,40 +1,8 @@
-let ogr1 = "Çetin Yılmaz"
+let yas = 18;
+let mezuniyet = "Lise";
 
-let ogr1Date = 2004;
-
-let ogr1Odev = 100;
-let ogr1Vize = 100;
-let ogr1Final = 100;
-
-let ogr1BilgisayarNotlari = ogr1Odev + ogr1Vize + ogr1Final ;
-
-
-console.log(ogr1 + "'ın" + " " + (2024 - ogr1Date) + " Yaşında" +
-" Ders Ortalması " + 
-(ogr1BilgisayarNotlari / 3) + 
-" Öğrenci Dersten Geçtimi ? : " + 
-( 45 <= (ogr1BilgisayarNotlari / 3)) );
-
-/* */
-
-let ogr2 = "Eylül Demir"
-
-let ogr2Date = 2022;
-
-let ogr2Odev = 60;
-let ogr2Vize = 20;
-let ogr2Final = 50;
-
-let ogr2BilgisayarNotlari = parseInt((ogr2Odev + ogr2Vize + ogr2Final) / 3) ;
-
-/* */
-
-console.log(ogr2 + "'ın" + " " + (2024 - ogr2Date) + " Yaşında" +
-" Ders Ortalması " + 
-ogr2BilgisayarNotlari + 
-" Öğrenci Dersten Geçtimi ? : " + 
-( 45 <= ogr2BilgisayarNotlari) );
-
-
-
-
+if(yas >= 18){
+    console.log("Ehliyet alabilirsin.");
+}else{
+    console.log("Ehliyet alamazsın.");
+}
