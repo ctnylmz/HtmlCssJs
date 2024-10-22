ef31d01029bdf39e01b6f496fce443c32b6bec50
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-10-22 10:28:47 +0300

    Javascript Object

---
+++ Dersler/1/assets/js/object.js
@@ -0,0 +1,34 @@
+let customer1 = {
+    "ad": "Çetin",
+    "soyad": "Yılmaz",
+    "sehir": "İstanbul",
+    "yas": "20",
+    "products": {
+        "Elektorink": "1",
+        "Giyim": "0"
+    }
+}
+
+let customer2 = {
+    "ad": "Emin",
+    "soyad": "Basbayan",
+    "sehir": "Ankara",
+    "yas": "25",
+    "products": {
+        "Elektorink": "0",
+        "Giyim": "3"
+    }
+}
+
+let customer3 = {
+    "ad": "Engin",
+    "soyad": "Demirog",
+    "sehir": "Ankara",
+    "yas": "30",
+    "products": {
+        "Elektorink": "3",
+        "Giyim": "0"
+    }
+}
+
+console.log(customer1);
