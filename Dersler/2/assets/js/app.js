61c6405232e9b2b6630801260a71c0443631349e
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-10-23 10:53:15 +0300

    Updated

--- Dersler/2/assets/js/app.js
+++ Dersler/2/assets/js/app.js
@@ -1,65 +1,45 @@
 const textInputDom = document.getElementById("todo");
-
 const btnAddTodoDom = document.querySelector(".create-btn");
-
 const TodosDom = document.getElementById("todos");
-
-
-
-
 
 let textInputValue = "";
 let todos = [];
 
 textInputDom.addEventListener("change", function (event) {
     textInputValue = event.target.value;
-})
+});
 
 btnAddTodoDom.addEventListener("click", function (e) {
     e.preventDefault();
 
-    todos.unshift({ id: todos.length + 1, todoTitle: textInputValue });
-
-    textInputDom.value = "";
-
-    displayTodos();
-})
+    if (textInputValue.trim() !== "") {
+        todos.unshift({ id: todos.length + 1, todoTitle: textInputValue });
+        textInputDom.value = "";
+        textInputValue = "";
+        displayTodos();
+    }
+});
 
 function displayTodos() {
     let result = "";
 
     todos.forEach((item) => {
-        result += 
-        `
+        result += `
           <li>
             <span>${item.todoTitle}</span>
-            <button class="delete-btn" onclick="deleteTodo(${item.id})">Delete${item.id}</button>
-        </li>
-        `
-        
+            <button class="delete-btn" onclick="deleteTodo(${item.id})">Delete</button>
+          </li>
+        `;
     });
     TodosDom.innerHTML = result;
 }
 
-
 function deleteTodo(id) {
-    let deletedId;
-
-    for (let i in todos) {
-        if (todos[i].id == id) {
-            deletedId = i
-        }
-    }
-
-    todos.splice(deletedId, 1)
-
+    todos = todos.filter((todo) => todo.id !== id);
     displayTodos();
-
 }
 
 function clearTodos() {
-
-    todos.splice(0, todos.length)
+    todos = [];
     displayTodos();
-
 }
