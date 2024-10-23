ed11369a932d92806944ad485d2e4eb89051cc91
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-10-23 10:34:36 +0300

    Javascript Finish
    Basic Todo List

--- Dersler/1/assets/js/app.js
+++ Dersler/1/assets/js/app.js
@@ -1,6 +1,70 @@
-const title = document.querySelector("#tit");
+const textInputDom = document.getElementById("todo");
 
-title.style.color = "red"
+const btnAddTodoDom = document.querySelector(".create-btn");
+
+const TodosDom = document.getElementById("todos");
+
+
+
+
+
+let textInputValue = "";
+let todos = [];
+
+textInputDom.addEventListener("change", function (event) {
+    textInputValue = event.target.value;
+})
+
+btnAddTodoDom.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    todos.unshift({ id: todos.length + 1, todoTitle: textInputValue });
+
+    textInputDom.value = "";
+
+    displayTodos();
+})
+
+function displayTodos() {
+    let result = "";
+
+    todos.forEach((item) => {
+        result += `
+          <li>
+            <span>${item.todoTitle}</span>
+            <button class="delete-btn" onclick="deleteTodo(${item.id})">Delete${item.id}</button>
+        </li>
+        `
+    });
+    TodosDom.innerHTML = result;
+}
+
+
+function deleteTodo(id) {
+    let deletedId;
+
+    for (let i in todos) {
+        if (todos[i].id == id) {
+            deletedId = i
+        }
+    }
+
+    todos.splice(deletedId, 1)
+
+    displayTodos();
+
+}
+
+function clearTodos() {
+
+    todos.splice(0, todos.length)
+    displayTodos();
+
+}
+
+
+
+
 
 
 
